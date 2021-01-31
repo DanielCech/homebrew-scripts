b5542994175fe9358a9fbbe47c7033397d053229
Daniel Cech <daniel.cech@strv.com>
2021-01-31 23:35:21 +0100

    [feat]: trying installation

--- codetemplater.rb
+++ codetemplater.rb
@@ -1,16 +1,16 @@
 class Codetemplater < Formula
     desc "CodeTemplater is command line tool for generating Swift source code from templates."
     homepage "https://github.com/strvcom/ios-code-templates"
-    url "https://github.com/DanielCech/homebrew-scripts/blob/main/Release/codeTemplater-0.1.1.tar.gz"
+    url "https://github.com/DanielCech/homebrew-scripts/blob/main/Release/codetemplater-0.1.1.tar.gz"
     sha256 "15b70148143b8de6f4cf87afd299a7875adfe65243c1f3f387ef7117595e8f33"
   
     bottle :unneeded
   
     def install
-      bin.install "codeTemplater"
+      bin.install "codetemplater"
     end
   
     test do
-      system "#{bin}/codeTemplater", "--help"
+      system "#{bin}/codetemplater", "--help"
     end
   end
