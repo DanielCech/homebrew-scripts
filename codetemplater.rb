3a66263d6e3b2e4c45bd06b330fbd1f6b9b375a3
Daniel Cech <daniel.cech@strv.com>
2021-02-02 16:35:11 +0100

    [feat]: new sha

--- codetemplater.rb
+++ codetemplater.rb
@@ -2,7 +2,7 @@ class Codetemplater < Formula
     desc "CodeTemplater is command line tool for generating Swift source code from templates."
     homepage "https://github.com/strvcom/ios-code-templates"
     url "https://github.com/DanielCech/homebrew-scripts/blob/main/Release/codetemplater-0.1.1.tar.gz"
-    sha256 "5263cd3599e633906a1c089164040184d81fa7d9679a19c2577663462c3944d5"
+    sha256 "77f13e9086a515bd829c348071635158e05519fb9fc7c0731d9352cf5564dc47"
   
     bottle :unneeded
   
