002e0f8a4fe852ce3994652f0218891563d8bdb4
Daniel Cech <daniel.cech@strv.com>
2021-03-31 08:37:24 +0200

    [feat]: stylesheethelper update

--- stylesheethelper.rb
+++ stylesheethelper.rb
@@ -1,8 +1,8 @@
 class Stylesheethelper < Formula
     desc "The script that exports stylesheet format from Figma CSS description"
     homepage "https://github.com/DanielCech/homebrew-scripts"
-    url "https://github.com/DanielCech/homebrew-scripts/releases/download/0.1.1/codetemplater-0.1.1.tar.gz"
-    sha256 "a626f2b7af7b2637642f7b2ec5b485017e17395e65ce242feaccdff3b8931997"
+    url "https://github.com/DanielCech/homebrew-scripts/releases/download/0.1.2/stylesheethelper-1.0.0.tar.gz"
+    sha256 "49c27009de47d26e5c53dc21c7bc68dac481795d87736e478dc99d9bf7557ee7"
   
     bottle :unneeded
   
